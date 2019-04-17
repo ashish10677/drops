704413880656c34e99cd0e3dc8a3a43616309adb
Ashish Kumar Mishra <ashish10677@gmail.com>
2019-04-17 10:19:28 +0530

    Add NavHeader
    Signed-off-by: Ashish Kumar Mishra <ashish10677@gmail.com>

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('files/', views.file_list, name="file_list"),
+    path('', views.file_list, name="file_list"),
     path('files/upload', views.upload_file, name='file_upload'),
 ]
